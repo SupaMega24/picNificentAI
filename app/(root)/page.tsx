a4fc581f89aa723b63e8eba45fa1d426772987a3
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-21 17:03:37 +0900

    Add header navbar for mobile devices

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
-import { UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
 
-      {/* <p>Home</p>
+      <p>Home</p>
 
-      <UserButton afterSignOutUrl='/' /> */}
+
 
     </div>
   )
