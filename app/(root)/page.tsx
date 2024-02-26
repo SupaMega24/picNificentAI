4b0850c5605eb5aa162aeff89f4451f49cfd26ae
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-26 21:52:32 +0900

    Update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,42 @@
 
+import { navLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
 
-      <p>Home</p>
+    <>
 
+      <section className='home'>
 
+        <h1 className='home-heading pb-2'>
+          Create Magnificent Images with Picnificent
+        </h1>
+        <ul className='flex-center w-full gap-20'>
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              href={link.route}
+              key={link.route}
+              className='flex-center flex-col gap-2'
+            >
+              <li className='flex-center w-fit rounded-full bg-white p-4'>
+                <Image
+                  src={link.icon}
+                  alt="image"
+                  width={24}
+                  height={24}
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
 
-    </div>
+      </section>
+
+    </>
   )
 }
 
